67db1850069aebc8e614b88d89cdbf1335c4e3df
cpkopp <cameronpkopp@gmail.com>
2022-12-05 18:15:48 -1000

    index db

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,34 +8,32 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    module: {
-        rules: [
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            ['@babel/preset-env', { targets: "defaults" }]
-                        ]
-                    }
-                }
-            }
-        ]
-    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './index.html',
             title: 'Webpack Plugin',
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']   
+                    }
+                }
+            }
+        ]
+    }
 };
--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -4,6 +4,6 @@ const path = require('path');
 //EXPORT GET ROUTE to send html file==========================
 module.exports = function(app) {
     app.get('/', function(req, res) {
-        res.sendFile(path.join(__dirname, '../../client/index.html'));
+        res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
 };
